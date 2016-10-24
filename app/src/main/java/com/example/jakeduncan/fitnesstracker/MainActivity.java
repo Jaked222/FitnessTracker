4c2fddae3b0c6cffc1f8e6c9d93ec1650e8c9e86
Jaked222 <Asphynx222@gmail.com>
2016-10-24 17:50:43 -0400

    verification query seems to be working properly. it even has defense against SQLinjection.

--- app/src/main/java/com/example/jakeduncan/fitnesstracker/MainActivity.java
+++ app/src/main/java/com/example/jakeduncan/fitnesstracker/MainActivity.java
@@ -1,17 +1,20 @@
 package com.example.jakeduncan.fitnesstracker;
 
 import android.content.ContentValues;
-import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
-import static android.R.attr.value;
 import static com.example.jakeduncan.fitnesstracker.R.id.pass;
+import static com.example.jakeduncan.fitnesstracker.UserTable.TABLE_NAME;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -39,22 +42,15 @@ public class MainActivity extends AppCompatActivity {
               passWord = passField.getText().toString();
 
 
-             //  sign(userName, passWord);
               SignInOrRegister(userName, passWord);
             }
         });
-    }
-    public void sign(String user, String pass){
-        Intent myIntent = new Intent(this, UserActivity.class);
-        myIntent.putExtra("key", value); //Optional parameters
-        startActivity(myIntent);
     }
     public void SignInOrRegister(String user, String pass){
         //check db for username. if doesnt exist, create it with given pass. Open fitness page from 0
         //if exists, open user info with tracked fitness info.
         setContentView(R.layout.database_list);
         ListView productList = (ListView) findViewById(R.id.products_list);
-
 
 
         DatabaseHelper databaseHelper = new DatabaseHelper(this);
@@ -64,12 +60,39 @@ public class MainActivity extends AppCompatActivity {
         values.put(UserTable.DISTANCE, 50);
         values.put(UserTable.PASSWORD, pass);
 
-        databaseHelper.getWritableDatabase().insert(UserTable.TABLE_NAME, null, values);
+        databaseHelper.getWritableDatabase().insert(TABLE_NAME, null, values);
+
+       if( verification("jakedddd")){
+
+           Log.d("yes", "true");
+       }else{
+           Log.d("no", "false ");
+       }
 
 
         String[] from = new String[] {UserTable.NAME, UserTable.DISTANCE, UserTable.PASSWORD};
         int[] to = new int[] {R.id.user_name, R.id.user_distance, R.id.user_pass};
 
         productList.setAdapter(new SimpleCursorAdapter(this, R.layout.product_row, databaseHelper.getProductCursor(), from, to));
+    }
+    public boolean verification(String _username) throws SQLException {
+        DatabaseHelper databaseHelper = new DatabaseHelper(this);
+        SQLiteDatabase dataBase = databaseHelper.getReadableDatabase();
+        int count = -1;
+        Cursor c = null;
+        try {
+            String query = "SELECT COUNT(*) FROM "
+                    + TABLE_NAME + " WHERE " + UserTable.NAME + " = ?";
+            c = dataBase.rawQuery(query, new String[] {_username});
+            if (c.moveToFirst()) {
+                count = c.getInt(0);
+            }
+            return count > 0;
+        }
+        finally {
+            if (c != null) {
+                c.close();
+            }
+        }
     }
 }
